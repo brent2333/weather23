3ea62edabaec15a997c06e0aa7681d0d3246b6ef
Brent Lawson <brent.lawson@fleetcor.com>
2023-01-06 15:07:34 -0500

    clashing classes lol

--- src/Results.tsx
+++ src/Results.tsx
@@ -6,19 +6,19 @@ const Results = ({ current }: CurrentWeatherResponse) => {
   return (
     <Fragment>
       <div className="grid grid-flow-col auto-cols-max h-48">
-        <div className="bg-white shadow-md rounded m-4 text-3xl font-bold center-flex px-12 bg-gray-300">
+        <div className="shadow-md rounded m-4 text-3xl font-bold center-flex px-12 bg-gray-300">
           {current?.condition.text}&nbsp;
           {current?.condition.icon ? (
           <img src={`https:${current?.condition.icon}`} alt={current?.condition.icon} />
           ) : null}
         </div>
         {current?.temp_f ? (
-          <div className="bg-white shadow-md rounded m-4 text-3xl font-extrabold center-flex px-12 bg-gray-300">
+          <div className="shadow-md rounded m-4 text-3xl font-extrabold center-flex px-12 bg-gray-300">
             {current?.temp_f} <span>&#176;</span>F
           </div>
         ) : null}
         {current?.temp_c ? (
-          <div className="bg-white shadow-md rounded  m-4 text-3xl font-extrabold center-flex px-12 bg-gray-300">
+          <div className="shadow-md rounded  m-4 text-3xl font-extrabold center-flex px-12 bg-gray-300">
             {current?.temp_c} <span>&#176;</span>C
           </div>
         ) : null}
