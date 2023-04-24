6c0aecf8a78aad5b08099095650ad01980107da9
Brent Lawson <brent.lawson@fleetcor.com>
2023-04-24 08:58:24 -0400

    edit about txt

--- src/About.tsx
+++ src/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
         <div className="max-w-lg my-0 mx-auto">
         <h1 className="text-lg font-bold w-full">About Me</h1>
         <p className="">Hello 😀. As you may have seen, my name is Brent Lawson. I have been developing for the Web since 2010. In my first gig working for others, I was the lone developer for an SEO company in Atlanta. In that role, I administered Apache servers, wrote PHP (Wordpress and otherwise) and all the usual front end HTML/CSS/JS stuff as well as created conversion recommendations and implemented them. After a while, I began to focus more on being a front-end dev with an eye on the latest developments in the JavaScript community and WCAG and excellent UX.</p>
-        <p className="max-w-md">These days I am into things like MERN stack, NextJS ExpressJS etc. My favorite experience and preference for creating a web front-end is React although I have built some stuff with Angular and Vue.</p>
+        <p className="max-w-md">These days I am into full-stack JS approaches, using NextJS & ExpressJS. I have alos taken a strong interest in creating Design Systems with React/Storybook/Figma. My favorite experience and preference for creating a web front-end is React although I have built some stuff with Angular and Vue.</p>
         </div>
     </div>
   );
